1cdeea687b37f95532e93123c23930ee7b8bcc50
vaishnav319 <68817230+vaishnav319@users.noreply.github.com>
2021-06-01 14:57:57 +0530

    Final app commit

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const fetch = require('node-fetch');
 const express=require("express");
 const app =express();
 app.set("view engine","ejs");
-
 app.use(express.static("public"));
-
+//get route
 app.get("/", async function(req,res){
   const response = await fetch("https://api.wazirx.com/api/v2/tickers")
   const data = await response.json();
   const keys = Object.keys(data);
   res.render("body",{data:data,keys:keys})
-
-
 })
-
+//Post route- here nothing to post
 app.post("/",function(req,res){
 })
-
-
-app.listen(3000,function(){
-  console.log("server is running on port 3000");
-})
+//Accessing port
+let port = process.env.PORT;
+if(port == null || port==""){
+  port=3000;
+}
+app.listen(port, function() {
+  console.log("Server started Successfully");
+});
